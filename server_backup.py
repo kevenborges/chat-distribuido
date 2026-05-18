dba463cef9b6bd1bfd8c0f73605df9903e73f4ca
Keven Borges <108688710+kevenborges@users.noreply.github.com>
2026-05-18 14:08:30 -0300

    Update server_backup.py

--- server_backup.py
+++ server_backup.py
@@ -2,9 +2,11 @@ import socket
 import threading
 
 HOST = '127.0.0.1' 
-PORT = 5005
-clientes = []
+PORT = 5005        # Porta de comunicação do Servidor de Backup
+clientes = []      # Lista global para armazenar os clientes que migrarem para cá
+
 def transmitir_mensagem(mensagem, cliente_remetente=None):
+    """Envia a mensagem recebida para todos os outros clientes conectados na porta 5005."""
     for cliente in clientes:
         if cliente != cliente_remetente:
             try:
@@ -13,7 +15,8 @@ def transmitir_mensagem(mensagem, cliente_remetente=None):
                 remover_cliente(cliente)
 
 def gerenciar_cliente(cliente_socket, endereco):
-    print(f"[SERVIDOR BACKUP] {endereco} conectado.")
+    """Escuta mensagens do cliente. Roda em uma thread separada."""
+    print(f"[SERVIDOR BACKUP] {endereco} assumido.")
     while True:
         try:
             mensagem = cliente_socket.recv(1024)
@@ -21,6 +24,8 @@ def gerenciar_cliente(cliente_socket, endereco):
                 break
             
             texto = mensagem.decode('utf-8', errors='ignore')
+            
+            # Filtro Anti-Lixo para ignorar robôs de monitoramento
             if "HTTP" in texto or "User-Agent" in texto or not texto.startswith('['):
                 continue
 
@@ -30,16 +35,19 @@ def gerenciar_cliente(cliente_socket, endereco):
     remover_cliente(cliente_socket)
 
 def remover_cliente(cliente_socket):
+    """Remove e fecha o socket do cliente."""
     if cliente_socket in clientes:
         clientes.remove(cliente_socket)
         cliente_socket.close()
 
 def iniciar_servidor():
+    """Inicializa o servidor de Backup e aguarda conexões de resgate (tolerância a falhas)."""
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     servidor.bind((HOST, PORT))
     servidor.listen()
     print(f"[RODANDO] Servidor BACKUP na porta {PORT}")
+    
     while True:
         cliente_socket, endereco = servidor.accept()
         clientes.append(cliente_socket)
